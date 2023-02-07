064c1bf2e0a65b9e3f1f7f1ed4e04cd4475b104e
Gustavo <guga6d@gmail.com>
2023-02-07 15:57:33 -0300

    Commit 5 dos meus estudos

---
+++ objeto/oo.js
@@ -0,0 +1,22 @@
+//NAO EXECUTAVEL
+
+//Procedural
+processamento(valor1,valor2,valor3)
+
+//OO = Orientacao Objeto
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        //...
+    }
+}
+
+objeto.processamento() // Foco passa a ser o objeto
+
+// Principios importantes:
+// 1. Abstracao
+// 2. Encapsulamento
+// 3. Heranca (prototype)
+// 4. Polimorfismo
