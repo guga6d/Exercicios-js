06c411e92ad468035f328de0f335a6a1fccbc828
Gustavo <guga6d@gmail.com>
2023-02-25 14:27:35 -0300

    Commit 15 dos meus estudos

--- array/filter2.js
+++ array/filter2.js
@@ -1,9 +1,20 @@
+Array.prototype.filter2 = function(callback) {
+    const newArray = []
+    for(let i =0; i< this.length; i++){
+        if(callback(this[i], i, this)){
+            newArray.push(this[i])
+        }
+    }
+    return newArray
+}
+
 const produtos = [
     {nome:'Notebook',preco:2499,fragil:true},
     {nome:'iPad Pro',preco:4199,fragil:true},
-    {nome:'Copo de vidro',preco:12.99,fragil:true},
+    {nome:'Copo de Vidro',preco:12.99,fragil:true},
     {nome:'Copo de Plastico',preco:18.99,fragil:false}
 ]
+
 const caro = produto => {
     if(produto.preco >= 500){
         return true
@@ -15,15 +26,12 @@ const fragil = produto => {
         return true
     }
 }
-const resultado = produtos.filter(caro).filter(fragil)
+const resultado = produtos.filter2(caro).filter2(fragil)
 
 console.log(resultado)
-
-//Resposta do professor abaixo
-
 
 
 const caro2 = produto => produto.preco >= 500
 const fragil2 = produto => produto.fragil
 
-console.log(produtos.filter(caro2).filter(fragil2))
+console.log(produtos.filter2(caro2).filter2(fragil2))
